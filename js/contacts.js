4c33ad3b0e5ea70a2cca226c14dcc742232da97b
wassim <wassim5700@gmail.com>
2018-03-19 12:06:13 +0000

    -- Ajout de contact : contient patie html , et partie css

---
+++ js/contacts.js
@@ -0,0 +1,81 @@
+/* 
+Activité : gestion des contacts
+*/
+
+// TODO : complétez le programme
+
+/*
+    N'hésitez surtout pas à faire des remarques et des critiques ; 
+    et merci pour votre correction.
+ */
+//prototype contact
+var contact = {
+    nom:"",
+    prenom:"",
+    afficherContact:function(){
+        console.log("contact : nom='"+this.nom+"' , prenom='"+this.prenom+"'");
+    },
+    init: function(nom,prenom){
+        this.nom=nom;
+        this.prenom=prenom;
+    }
+}
+
+//tableau des contacts
+var allContacts =[];
+
+//fonction pour ajouter un contact
+var ajouterContact=function(nom,prenom){
+        var newContact= Object.create(contact);
+        newContact.init(nom,prenom);
+        allContacts.push(newContact);
+}
+
+//fonction pour afficher tou les contacts du tableau (optionel)
+var afficherToutContacts=function(){
+    allContacts.forEach(function(element) {
+        element.afficherContact();
+    });
+} 
+
+
+//les 2 contacts à inserer initialement 
+ajouterContact("Lévisse","Carole");
+ajouterContact("Nelsonne","Mélodie");
+
+//mesage d accueil
+var messageAccueil = "Bienvenue dans le gestionnaire de contacts";
+console.log(messageAccueil);
+
+var option=3; //Le choix de 3 est utilisé juste pour initialiser la variable choix et n'a aucun sens ;-)
+
+while(option!==0){
+    console.log(" 1 : Lister les contacts \n 2 : Ajouter un contact \n 3 : Quitter ");
+    option= prompt("Choisissez une option: ").trim();   //trim : pour ne pas prendre d'espace utile surtout pour la condition suivante
+    if(option!==""){    // Cette condition est faite pour que la chaine vide "" ne soit pas convertit en 0
+        option=Number(option);
+    }
+    if(option===0){
+        //quitter
+        console.log("bye bye!");
+        alert("bye bye!");
+    }else{
+        if(option===1){
+            //lister les contacts
+            console.log("Voici la liste de tous vos contacts :");
+            afficherToutContacts();
+        }else{
+            if(option===2){
+                //ajouer nv contact
+                var nvNom=prompt("entrez le nom du nouveau contact :");
+                var nvPrenom=prompt("entrez le prenom du nouveau contact :");
+                ajouterContact(nvNom,nvPrenom);
+                console.log("nouveau con tact ajouté avec succes !");
+            }
+            else{
+                console.log("choix non pris en charge!");
+                alert("choix non pris en charge!");
+            }
+        }
+    }
+}
